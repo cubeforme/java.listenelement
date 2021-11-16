7fc20925767190cdfca50771851a83c401a00fdb
cubeforme <81507460+cubeforme@users.noreply.github.com>
2021-11-16 23:32:24 +0100

    Delete warteschlange-liste-knoten-listenelement directory

--- warteschlange-liste-knoten-listenelement/src/warteschlange/liste/knoten/listenelement/LISTENELEMENT.java
+++
@@ -1,9 +0,0 @@
-package warteschlange.liste.knoten.listenelement;
-public abstract class LISTENELEMENT {
-    public abstract LISTENELEMENT hintenEinfuegen(DATENELEMENT dneu);
-    public abstract LISTENELEMENT nachfolgerGeben();
-    public abstract DATENELEMENT inhaltGeben();
-    public abstract String letzterNameGeben(DATENELEMENT d);
-    public abstract boolean nameVorhanden(String gesuchterName);
-    public abstract void alleAusgeben();
-}
